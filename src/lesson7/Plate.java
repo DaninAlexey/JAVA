bac81fce78ece33e97a1c17f7934930eb446a4b6
DaninAlexey <dalex772@yandex.ru>
2021-10-26 02:36:34 +0700

    ДЗ - 7. Курс Java-1

---
+++ src/lesson7/Plate.java
@@ -0,0 +1,29 @@
+package lesson7;
+
+public class Plate {
+    private int food;
+
+   public Plate(int food) {
+        this.food = food;
+    }
+
+    public void plateinfo()
+    {
+        System.out.println("В тарелке " + food + " еды");
+    }
+
+    public void addFood(int afood)
+    {  if(afood>=0)
+     food += afood;
+    }
+
+    public boolean decreaseFood(int dfood)
+    {
+        if(food - dfood < 0)
+            return false;
+            else{
+                food -=dfood;
+                return true;
+        }
+    }
+}
